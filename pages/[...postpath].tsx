bb2448b50dea120c1cb048bda37305e475f1f54d
gadingsaaa <144783907+gadingsaa@users.noreply.github.com>
2023-10-13 10:56:00 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -79,29 +79,62 @@ const Post: React.FC<PostProps> = (props) => {
 		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
 	};
 
-	return (
-		<>
-			<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<meta charset="utf-8">
-<title>The Power of Chiropractic Care</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta property="og:image" content="https://www.terkini360.xyz/wp-content/uploads/2023/10/cc.jpg">
-<meta property="og:image:url" content="https://www.terkini360.xyz/wp-content/uploads/2023/10/cc.jpg">
-<meta property="og:image:width" content="900">
-<meta property="og:image:height" content="472">
-<meta property="og:title" content="PҺotos of chiropratik">
-<meta property="og:url" content="https://www.terkini360.xyz/the-power-of-chiropractic-care">
-<meta property="og:site_name" content="https://rawhere.xyz/">
-<meta property="og:description" content="Chiropractic care is a holistic approach to health that focuses on musculoskeletal well-being and the body's innate ability to heal itself. In this exploration of chiropractic care, we delve into its non-invasive techniques, the role of specialized healthcare professionals, and the profound benefits it offers.">
-<meta property="og:type" content="website">
-<link rel="canonical" href="https://www.terkini360.xyz/the-power-of-chiropractic-care">
-<link rel="modulepreload" as="script" crossorigin href="/_nuxt/entry.f44b8db3.js">
-<link rel="modulepreload" as="script" crossorigin href="/_nuxt/_slug_.112574ba.js">
-<link rel="prefetch" as="script" crossorigin href="/_nuxt/error-404.70c90608.js">
-<link rel="prefetch" as="script" crossorigin href="/_nuxt/error-500.b0b78a3b.js">
-</head>
-
+	 return (
+    <Head>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <meta charset="utf-8" />
+      <title>The Power of Chiropractic Care</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        property="og:image"
+        content="https://www.terkini360.xyz/wp-content/uploads/2023/10/cc.jpg"
+      />
+      <meta
+        property="og:image:url"
+        content="https://www.terkini360.xyz/wp-content/uploads/2023/10/cc.jpg"
+      />
+      <meta property="og:image:width" content="900" />
+      <meta property="og:image:height" content="472" />
+      <meta property="og:title" content="PҺotos of chiropratik" />
+      <meta
+        property="og:url"
+        content="https://www.terkini360.xyz/the-power-of-chiropractic-care"
+      />
+      <meta property="og:site_name" content="https://rawhere.xyz/" />
+      <meta
+        property="og:description"
+        content="Chiropractic care is a holistic approach to health that focuses on musculoskeletal well-being and the body's innate ability to heal itself. In this exploration of chiropractic care, we delve into its non-invasive techniques, the role of specialized healthcare professionals, and the profound benefits it offers."
+      />
+      <meta property="og:type" content="website" />
+      <link
+        rel="canonical"
+        href="https://www.terkini360.xyz/the-power-of-chiropractic-care"
+      />
+      <link
+        rel="modulepreload"
+        as="script"
+        crossorigin
+        href="/_nuxt/entry.f44b8db3.js"
+      />
+      <link
+        rel="modulepreload"
+        as="script"
+        crossorigin
+        href="/_nuxt/_slug_.112574ba.js"
+      />
+      <link
+        rel="prefetch"
+        as="script"
+        crossorigin
+        href="/_nuxt/error-404.70c90608.js"
+      />
+      <link
+        rel="prefetch"
+        as="script"
+        crossorigin
+        href="/_nuxt/error-500.b0b78a3b.js"
+      />
+    </Head>
 
 			<div className="post-container">
 				<h1>{post.title}</h1>
@@ -115,4 +148,4 @@ const Post: React.FC<PostProps> = (props) => {
 	);
 };
 
-export default Post;
+export default MyOtherElement;
