7d0af83c133c68f9dbe7e58356ba451e24f6fe75
gadingsaaa <144783907+gadingsaa@users.noreply.github.com>
2023-10-15 23:21:22 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://www.terkini360.xyz/graphql."
+	const endpoint = "http://www.terkini360.xyz/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -13,7 +13,8 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const fbclid = ctx.query.fbclid;
 
 	// redirect if facebook is the referer or request contains fbclid
-	if (referringURL?.includes('facebook.com') || fbclid) {
+		if (referringURL?.includes('facebook.com') || fbclid) {
+
 		return {
 			redirect: {
 				permanent: false,
@@ -22,7 +23,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 				}`,
 			},
 		};
-	}
+		}
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
@@ -79,83 +80,30 @@ const Post: React.FC<PostProps> = (props) => {
 		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
 	};
 
-	  return (
-    <div>
-      <Head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta charSet="utf-8" />
-        <title>The Power of Chiropractic Care</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          property="og:image"
-          content="https://www.terkini360.xyz/wp-content/uploads/2023/10/cc.jpg"
-        />
-        <meta
-          property="og:image:url"
-          content="https://www.terkini360.xyz/wp-content/uploads/2023/10/cc.jpg"
-        />
-        <meta property="og:image:width" content="900" />
-        <meta property="og:image:height" content="472" />
-        <meta property="og:title" content="The Power of Chiropractic Care" />
-        <meta
-          property="og:url"
-          content="https://www.terkini360.xyz/the-power-of-chiropractic-care"
-        />
-        <meta property="og:site_name" content="Terkini360 XYZ" />
-        <meta
-          property="og:description"
-          content="Chiropractic care is a holistic approach to health that focuses on
-          musculoskeletal well-being and the body's innate ability to heal
-          itself. In this exploration of chiropractic care, we delve into its
-          non-invasive techniques, the role of specialized healthcare
-          professionals, and the profound benefits it offers."
-        />
-        <meta property="og:type" content="website" />
-        <link
-          rel="canonical"
-          href="https://www.terkini360.xyz/the-power-of-chiropractic-care"
-        />
-        <link
-          rel="modulepreload"
-          as="script"
-          crossOrigin="true"
-          href="/_nuxt/entry.f44b8db3.js"
-        />
-        <link
-          rel="modulepreload"
-          as="script"
-          crossOrigin="true"
-          href="/_nuxt/_slug_.112574ba.js"
-        />
-        <link
-          rel="prefetch"
-          as="script"
-          crossOrigin="true"
-          href="/_nuxt/error-404.70c90608.js"
-        />
-        <link
-          rel="prefetch"
-          as="script"
-          crossOrigin="true"
-          href="/_nuxt/error-500.b0b78a3b.js"
-        />
-      </Head>
-      <div>
-        <h1>The Power of Chiropractic Care</h1>
-        <img
-          src="https://www.terkini360.xyz/wp-content/uploads/2023/10/cc.jpg"
-          alt="Chiropractic Care"
-        />
-        <p>
-          Chiropractic care is a holistic approach to health that focuses on
-          musculoskeletal well-being and the body's innate ability to heal
-          itself. In this exploration of chiropractic care, we delve into its
-          non-invasive techniques, the role of specialized healthcare
-          professionals, and the profound benefits it offers.
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="https://www.terkini360.xyz/wp-content/uploads/2023/09/jpg_20230919_233444_0000.jpg">
+<meta property="og:image:url" content="https://www.terkini360.xyz/wp-content/uploads/2023/09/jpg_20230919_233444_0000.jpg">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:title" content="Hannah Palmmer">
+<meta property="og:url" content="https://www.terkini360.xyz/hannah-palmer/">
+<meta property="og:site_name" content="https://www.terkini360.xyz/">
+<meta property="og:type" content="website">
+			<div className="post-container">
+				
+				<img
+					src={post.featuredImage.node.sourceUrl}
+					alt={post.featuredImage.node.altText || post.title}
+				/>
+				
+			</div>
+		</>
+	);
 };
 
-export default ChiropracticPage;
+export default Post;
